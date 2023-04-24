9ee6878ad04815634a523957472bd01f84116db3
crispusomollo <crispusomollo@gmail.com>
2023-04-24 13:30:18 -0700

    Main header file

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @p: pointer to print functions
+ *
+ * Description: struct to store pointers
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*p)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+
+
+#endif
