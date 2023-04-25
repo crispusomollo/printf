edc6f5b3e6bb04885a5e13c585ceefbd5c4ac648
crispusomollo <crispusomollo@gmail.com>
2023-04-25 12:51:01 -0700

    printf main header

---
+++ handle_print.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * o_handle_print - print an argument based on type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array.
+ * @flags: Calculate active flags
+ * @width: get the width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ *
+ */
+
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', o_print_char}, {'s', o_print_string}, {'%', o_print_percent},
+		{'i', o_print_int}, {'d', o_print_int}, {'b', o_print_binary},
+		{'u', o_print_unsigned}, {'o', o_print_octal}, {'x', o_print_hexadecimal},
+		{'X', o_print_hexa_upper}, {'p', o_print_pointer}, {'S', o_print_non_printable},
+		{'r', o_print_reverse}, {'R', o_print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
